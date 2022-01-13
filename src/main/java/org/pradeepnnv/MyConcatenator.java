342b595058225c5a2199ca39dc28c5b53aae2f35
pradeepnnv <pradeepnnv@gmail.com>
2022-01-12 18:07:56 -0600

    First commit

---
+++ src/main/java/org/pradeepnnv/MyConcatenator.java
@@ -0,0 +1,23 @@
+package org.pradeepnnv;
+
+import java.util.Locale;
+
+public class MyConcatenator {
+    public static String concatanate(String ... strings){
+        StringBuilder builder = new StringBuilder();
+
+        for (int i=0; i<strings.length; i++){
+            if (i > 0){
+                builder.append(",");
+            }
+            builder.append(strings[i]);
+        }
+
+        return builder.toString();
+    }
+
+    public static String returnUpper(String input) {
+        return input.toUpperCase(Locale.ROOT);
+    }
+
+}
